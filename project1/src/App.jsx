abbe57d3df79a65d9bc7ec174ecc879312ea1470
Marcelo Victor <marcelovictorsm077@gmail.com>
2023-03-24 14:07:33 -0300

    Sidebar added

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -1,21 +1,30 @@
 import { Post } from "./Post"
 import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
+
+import styles from "./App.module.css"
 
 import "./global.css"
+
 
 export function App() {
   return (
     <div>
       <Header />
 
-      <Post 
-      author="Marcelo Victor"
-      content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quia magnam officiis quaerat voluptate maiores, eveniet nisi. Sed quod incidunt adipisci, quas, voluptate nisi magnam quis molestiae similique odit accusamus?"
-    />
-    <Post 
-      author="Fulano de tal"
-      content="Um novo post muito legal"
-    />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+        <Post 
+          author="Marcelo Victor"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quia magnam officiis quaerat voluptate maiores, eveniet nisi. Sed quod incidunt adipisci, quas, voluptate nisi magnam quis molestiae similique odit accusamus?"
+        />
+        <Post 
+          author="Fulano de tal"
+          content="Um novo post muito legal"
+        />
+        </main>
+      </div>
     </div>
   )
 }
---
+++ project1/src/components/Sidebar.jsx
@@ -0,0 +1,23 @@
+import styles from './Sidebar.module.css'
+
+export const Sidebar = () => {
+  return(
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.cover} 
+        src="https://images.unsplash.com/photo-1584949091598-c31daaaa4aa9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40" 
+      />
+
+      <div className={styles.profile}>
+        <strong>Marcelo Victor</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
