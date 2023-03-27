92c12bff607f564324c6367a90f99b62b3ed8e0a
Marcelo Victor <marcelovictorsm077@gmail.com>
2023-03-27 11:55:11 -0300

    Post finished

--- project1/src/components/Post.jsx
+++ project1/src/components/Post.jsx
@@ -29,8 +29,18 @@ export const Post = () => {
         <a href="">#nlw</a>
         <a href="">#rocketseat</a>
       </p>
-
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe o seu feedback</strong>
+        <textarea 
+          placeholder='Deixe um comentário'
+        />
+      <footer>
+        <button type='submit'>Publicar</button>
+      </footer>
+
+      </form>
     </article>
   )
 }
