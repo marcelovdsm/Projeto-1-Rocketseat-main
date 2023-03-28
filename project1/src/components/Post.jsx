9ea772dbfa24b9f288db7421179c8931f682a634
Marcelo Victor <marcelovictorsm077@gmail.com>
2023-03-28 16:09:48 -0300

    Content files

--- project1/src/components/Post.jsx
+++ project1/src/components/Post.jsx
@@ -4,7 +4,7 @@ import { Comment } from './Comment'
 import styles from './Post.module.css'
 import { Avatar } from './Avatar'
 
-export const Post = ({ author, publishedAt }) => {
+export const Post = ({ author, publishedAt, content }) => {
 
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR
@@ -32,19 +32,13 @@ export const Post = ({ author, publishedAt }) => {
       </header>
 
       <div className={styles.content}>
-      <p>Fala galeraa 👋</p>
-
-      <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-      <p>
-        <a href="">jane.design/doctorcare</a>
-      </p>
-
-      <p>
-        <a href="">#novoprojeto</a>
-        <a href="">#nlw</a>
-        <a href="">#rocketseat</a>
-      </p>
+        {content.map(line => {
+          if (line.type === 'paragraph') {
+            return <p>{line.content}</p>
+          } else if(line.type === 'link') {
+            return <p> <a href="">{line.content}</a> </p>
+          }
+        })}
       </div>
 
       <form className={styles.commentForm}>
